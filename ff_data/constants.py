63cdb669fe446a489e739e2702b51dd00e95795e
Shane Hampton <shanehampton1@gmail.com>
2023-03-01 18:51:09 -0500

    adding a basic jupyter notebook

--- ff_data/constants.py
+++ ff_data/constants.py
@@ -1,7 +1,7 @@
 SCORING_FORMAT = "ppr"
 
 # number of players, sorted by total socring desc, to scrape for a given year
-SCRAPE_PLAYER_COUNT = 250
+SCRAPE_PLAYER_COUNT = 10
 
 # number of games to include in stat window for rolling per-game stats
 STAT_LOOKBACK_GAMES = 24
